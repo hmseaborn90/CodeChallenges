9c74512967734584f4ce9b78ab031c06a2969f53
Harrison <hmseaborn@gmail.com>
2020-05-23 19:44:12 -0700

    need to rename current but..
    trying to do daily code challenge. Equal paren is a pair programing challenge

--- Challenges/current.js
+++ Challenges/current.js
@@ -1,7 +1,47 @@
+// version 1 was the way to go
+
 function maxTotal(nums) {
   console.log(nums);
   var fiveLargest = nums.sort().splice(5, 10);
   return fiveLargest.reduce(function (a, b) {
     return a + b;
   });
+}
+//v2 fail
+
+function maxTotal(nums) {
+  largest = [];
+  notLargest = [];
+  nums.map((num) => {
+    if (num < 0 && notLargest.length < 5) {
+      notLargest.push(num);
+    } else if (largest.length < 5) {
+      largest.push(num);
+    } else {
+      notLargest.push(num);
+    }
+  });
+  console.log("l", largest);
+  console.log("nl", notLargest);
+  return notLargest.reduce(function (a, b) {
+    return a + b;
+  });
+}
+
+//v3 fail
+function maxTotal(nums) {
+  console.log(nums);
+  var fiveL = nums.sort();
+  for (i = fiveL.length - 1; i >= 5; i--) {
+    if (fiveL[i] < 0) {
+      fiveL.pop(fiveL[i]);
+    }
+  }
+  console.log(fiveL.sort());
+  return fiveL
+    .sort()
+    .splice(5, 10)
+    .reduce(function (a, b) {
+      return a + b;
+    });
 }
