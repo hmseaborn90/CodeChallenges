fa71600bc654cca11fd0b0a2976ded54c35239ac
Harrison <hmseaborn90.gmail.com>
2024-05-21 09:58:46 -0700

    initialized new repo

--- Challenges/EqualParens.js
+++ Challenges/EqualParens.js
@@ -1,23 +1,24 @@
 function isValid(str) {
-  // WRITE YOUR CODE HERE!
-  let stack = [];
-  let openPar = {
-    "(": ")",
-  };
-  if (str === null) {
-    return false;
-  }
+	// WRITE YOUR CODE HERE!
+	let stack = [];
+	let openPar = {
+		"(": ")",
+	};
+	if (str === null) {
+		return false;
+	}
 
-  for (let i = 0; i < str.length; i++) {
-    let par = str[i];
-    if (str[i] == "(") {
-      stack.push(par);
-    } else if ([par]) {
-      let lastInStack = stack.pop();
-      if (openPar[lastInStack] !== par) return false;
-    }
-  }
-  return stack.length === 0;
+	for (let i = 0; i < str.length; i++) {
+		let par = str[i];
+		if (str[i] == "(") {
+			stack.push(par);
+		} else if ([par]) {
+			let lastInStack = stack.pop();
+			if (openPar[lastInStack] !== par) return false;
+		}
+	}
+	return stack.length === 0;
 }
 
 console.log(isValid("((()))"));
+//comment to push
